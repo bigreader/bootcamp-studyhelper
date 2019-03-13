72774970d5d2eb7e6021585a85dbd2b5593615e4
Joshua <bigreaderjd1996@gmail.com>
2019-03-12 21:20:23 -0500

    set up and link firebase

---
+++ assets/js/common.js
@@ -0,0 +1,11 @@
+var config = {
+	apiKey: "AIzaSyAijZwQ0G6vrh39yUy9ppnTW0ra568N-34",
+	authDomain: "bootcamp-studyhelper.firebaseapp.com",
+	databaseURL: "https://bootcamp-studyhelper.firebaseio.com",
+	projectId: "bootcamp-studyhelper",
+	storageBucket: "bootcamp-studyhelper.appspot.com",
+	messagingSenderId: "17529524535"
+};
+firebase.initializeApp(config);
+
+database = firebase.database();
