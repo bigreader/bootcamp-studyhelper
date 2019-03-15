71e01d0b1c0f592da13d2436ff7898d1f87d678d
Joshua <bigreaderjd1996@gmail.com>
2019-03-14 20:09:49 -0500

    fix formatting, typo

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,15 +1,15 @@
 function formatTime(sec) {
 	if (sec > 3600) {
-		return (Math.floor(sec/3600) + ":" + Math.floor(sec%3600/60).padStart(2, "0") + ":" + (sec%60).padStart(2, "0"));
+		return ((Math.floor(sec/3600) + "") + ":" + (Math.floor(sec%3600/60) + "").padStart(2, "0") + ":" + (sec%60 + "").padStart(2, "0"));
 	} else if (sec > 60) {
-		return (Math.floor(sec/60) + ":" + (sec%60).padStart(2, "0"));
-	} else if (sec > 3600) {
-		return ("0:" + (sec%60).padStart(2, "0"));
+		return ((Math.floor(sec/60) + "") + ":" + (sec%60 + "").padStart(2, "0"));
+	} else {
+		return ("0:" + (sec%60 + "").padStart(2, "0"));
 	}
 }
 
 var stopwatch = {
-	callbackInterval: null;
+	callbackInterval: null,
 
 	time: function() {
 		var startTime = localStorage.getItem("stopwatch");
@@ -21,7 +21,7 @@ var stopwatch = {
 	},
 
 	interval: function(callback) {
-		callbackInterval = setInterval(callback, 100);
+		callbackInterval = setInterval(callback, 500);
 	},
 
 	clearInterval: function() {
