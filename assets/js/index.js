6665ab53567f37d6615f737a7c069243cda341a5
Joshua <bigreaderjd1996@gmail.com>
2019-03-16 14:14:55 -0500

    track session running state

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
+var sessionRunning = false;
+
 $(document).ready(function() {
 
 	$('#start').on('click', function() {
+		//only start a session if we haven't started one already
+		if (sessionRunning) return;
+		sessionRunning = true;
+
 		stopwatch.reset();
 		stopwatch.interval(function() {
 			$('#timer-display').text(formatTime(stopwatch.time()));
@@ -8,11 +14,19 @@ $(document).ready(function() {
 	});
 
 	$('#reset').on('click', function() {
+		// we don't really care whether a session is running
+		sessionRunning = false;
+
 		stopwatch.clearInterval();
+
 		$('#timer-display').text(formatTime(0));
 	});
 
 	$('#stop').on('click', function() {
+		//only stop a session if we have one running
+		if (!sessionRunning) return;
+		sessionRunning = false;
+
 		stopwatch.clearInterval();
 		var time = stopwatch.time();
 		// save the time right now, so firebase lag doesn't matter
